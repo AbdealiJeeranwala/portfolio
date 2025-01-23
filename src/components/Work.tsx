3200c178ec7ffc2affcaeecc88ed3c1b06cbb159
Abdeali Jeeranwala <abdealipoonawala20@gmail.com>
2025-01-23 18:41:07 +0530

    Fix GSAP imports and TypeScript errors

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,46 +2,9 @@ import "./styles/Work.css";
 import WorkImage from "./WorkImage";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
 import { MdArrowOutward } from "react-icons/md";
 
-gsap.registerPlugin(useGSAP);
-
 const Work = () => {
-  useGSAP(() => {
-    let translateX: number = 0;
-    function setTranslateX() {
-      const box = document.getElementsByClassName("work-box");
-      const rectLeft = document
-        .querySelector(".work-container")!
-        .getBoundingClientRect().left;
-      const rect = box[0].getBoundingClientRect();
-      const parentWidth = box[0].parentElement!.getBoundingClientRect().width;
-      let padding: number =
-        parseInt(window.getComputedStyle(box[0]).padding) / 2;
-      translateX = rect.width * box.length - (rectLeft + parentWidth) + padding;
-    }
-
-    setTranslateX();
-
-    let timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".work-section",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
-        pin: true,
-        pinType: !ScrollTrigger.isTouch ? "transform" : "fixed",
-        id: "work",
-      },
-    });
-
-    timeline.to(".work-flex", {
-      x: -translateX,
-      duration: 40,
-      delay: 0.2,
-    });
-  }, []);
   return (
     <div className="work-section" id="work">
       <div className="work-container section-container">
